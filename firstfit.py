1a3b4cdb3a6605c2db3f8ccddf0f5e396361823e
pawel150199 <pawel.polski99@gmail.com>
2023-12-28 01:14:02 +0100

    update firstfit - pawel

--- firstfit.py
+++ firstfit.py
@@ -26,7 +26,7 @@ class FirstFit(Base):
         
     def find_path(self):
         for i in range(self.bitrates_num):
-            for demand in self.demands:
+            for demand_idx, demand in enumerate(self.demands):
                 idx = self.nodes_mapping(demand.node_in, demand.node_out)
                 path = self.paths[idx]
                 out = None
@@ -35,10 +35,22 @@ class FirstFit(Base):
                 for p in path:
                     distance = np.sum(self.distances[p == 1])
                     path_idx = np.where(p == 1)[0]
-                    num_slots = self.choose_slots_num(distance, demand.bitrates[i])
-                    out = self._allocate_slots(num_slots, path_idx)
-                    if out == True:
-                        break
+                    if i == 0:
+                        num_slots = self.choose_slots_num(distance, demand.bitrates[i])
+                        out = self._allocate_slots(num_slots, path_idx, demand_idx)
+                        if out == True:
+                                break
+
+                    else:
+                        num_slots_prev = self.choose_slots_num(distance, demand.bitrates[i-1])
+                        num_slots = self.choose_slots_num(distance, demand.bitrates[i])
+
+                        if num_slots != num_slots_prev:
+                            self._release_slots(path_idx, demand_idx)
+                            out = self._allocate_slots(num_slots, path_idx, demand_idx)
+
+                        if out == True:
+                            break
 
                 if out == False:
                     self.blocked += 1
@@ -46,24 +58,29 @@ class FirstFit(Base):
             print(f"Demands: {len(self.demands)}")
             print(f"Blocked Count: {self.blocked}")
             print(f"Blocked Ratio: {self.blocked/len(self.demands)}")
-            exit("END...")
+        print("Final results:\n")
+        print(f"Blocked requests: {self.blocked}")
+        print(f"Blocked ratio: {self.blocked/len(self.demands)}")
+        print(f"Allocated Slots: {np.count_nonzero(self.slots==0)}")
+        print(f"Allocated Slots: {np.count_nonzero(self.slots == 0)/(320*36)}")
                 
 
-    def _allocate_slots(self, num_slots: int, path_idx: np.array) -> bool:
+    def _allocate_slots(self, num_slots: int, path_idx: np.array, demand_idx: int) -> bool:
         """Allocate slots"""
-        slots = np.bitwise_or.reduce(self.slots[path_idx].astype(int), axis=0)
+        slots = np.where(self.slots[path_idx] > 0, 1, 0)
+        slots = np.bitwise_and.reduce(slots.astype(int), axis=0)
         idx = np.where(np.convolve(slots, np.ones(num_slots), mode='valid') == 0)[0]
 
         if idx.size == 0:
             return False
         else:
             start_idx = idx[0]
-            self.slots[path_idx, start_idx:start_idx + num_slots] = 1
+            self.slots[path_idx, start_idx:start_idx + num_slots] = demand_idx
             return True
 
-    def _release_slots(self):
-        pass
-
+    def _release_slots(self, path_idx: np.array, demand_idx: int):
+        self.slots[path_idx] = np.where(self.slots[path_idx] == demand_idx, 0, self.slots[path_idx])
+        
 if __name__ == "__main__":
     demands = load_demands("POL12/demands_6")
     paths = load_paths("POL12/pol12.pat")
